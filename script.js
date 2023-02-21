ebd1cfa23d2d5c2a57a11a7d48d280a00429e182
Emily Zepeda <emilyazepeda@gmail.com>
2023-02-21 15:23:20 -0600

    Finish game and reset

--- script.js
+++ script.js
@@ -1,29 +1,107 @@
-const player = (value, symbol) => {
-  this.value = value;
-  this.symbol = symbol;
-  const indices = [];
-
-  return { value, symbol, indices };
-};
-
 const form = document.querySelector('#player-info');
+
 form.addEventListener('submit', (event) => {
   event.preventDefault();
 
   const formData = new FormData(form);
   const data = Object.fromEntries(formData);
-  document.querySelector('.info').setAttribute('hidden', true);
-  console.log(data);
+  document.querySelector('.info').hidden = true;
+  startGame(data);
 });
 
-const tiles = document.querySelectorAll('.tile');
+const startGame = (data) => {
+  initializeVariables(data);
+  addEventListenersToGameBoard(data);
+};
 
-tiles.forEach((tile) => {
-  tile.addEventListener('click', () => {
-    addMove(tile);
-    if (window()) {
-      return `${turn} wins!`;
-    }
-    changeTurn();
+const initializeVariables = (data) => {
+  data.choice = +data.choice;
+  data.board = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+  data.player1 = 'X';
+  data.player2 = 'O';
+  data.round = 0;
+  data.currentPlayer = 'X';
+  data.gameOver = false;
+};
+
+const addEventListenersToGameBoard = (data) => {
+  document.querySelectorAll('.tile').forEach((tile) => {
+    tile.addEventListener('click', (event) => {
+      playMove(event.target, data);
+    });
   });
-});
+};
+
+const playMove = (tile, data) => {
+  if (data.gameOver) {
+    return;
+  }
+  if (data.board[tile.getAttribute('value')] !== 'X' && data.board[tile.getAttribute('value')] === 'O') {
+    return;
+  }
+  data.board[tile.getAttribute('value')] = data.currentPlayer;
+  console.log(data.board);
+  tile.textContent = data.currentPlayer;
+
+  if (data.round >= 4) {
+    checkWin(data);
+  } else {
+    switchTurn(data);
+  }
+};
+
+const checkWin = (data) => {
+  console.log(data.board);
+  if (data.board[0] === data.currentPlayer
+    && data.board[1] === data.currentPlayer
+    && data.board[2] === data.currentPlayer
+    || data.board[3] === data.currentPlayer
+    && data.board[4] === data.currentPlayer
+    && data.board[5] === data.currentPlayer
+    || data.board[6] === data.currentPlayer
+    && data.board[7] === data.currentPlayer
+    && data.board[8] === data.currentPlayer
+    || data.board[0] === data.currentPlayer
+    && data.board[3] === data.currentPlayer
+    && data.board[6] === data.currentPlayer
+    || data.board[1] === data.currentPlayer
+    && data.board[4] === data.currentPlayer
+    && data.board[7] === data.currentPlayer
+    || data.board[2] === data.currentPlayer
+    && data.board[5] === data.currentPlayer
+    && data.board[8] === data.currentPlayer
+    || data.board[0] === data.currentPlayer
+    && data.board[4] === data.currentPlayer
+    && data.board[8] === data.currentPlayer
+    || data.board[2] === data.currentPlayer
+    && data.board[4] === data.currentPlayer
+    && data.board[6] === data.currentPlayer) {
+    alertWin(data);
+    resetGame();
+  } else {
+    switchTurn(data);
+  }
+};
+
+const alertWin = (data) => {
+  document.querySelector('.gameboard').hidden = true;
+  let winner;
+  if (data.currentPlayer == data.player1) {
+    winner = 'Player 1';
+  } else {
+    winner = 'Player 2';
+  }
+
+  alert(`Congrats ${winner} wins`);
+  resetGame();
+};
+
+const switchTurn = (data) => {
+  data.round++;
+  data.currentPlayer = data.currentPlayer === 'X' ? 'O' : 'X';
+};
+
+const resetGame = () => {
+  location.reload();
+  // document.querySelector('.info').hidden = false;
+};
